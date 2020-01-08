f01064ccd4849633532ce20f7f35d58b43144fff
Martins Kozlovskis <mkozlovskis@gmail.com>
2020-01-08 15:58:17 -0600

    added lanuge support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,41 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stop-corruption-web-app';
+export class AppComponent implements OnInit, OnDestroy {
+  languages: string[];
+  languageNames = {
+    en: 'English',
+    lv: 'Latviešu',
+  };
 
-  constructor(translate: TranslateService) {
+  private availableLanguages = ['lv', 'en'];
+  private sub: Subscription;
+
+  constructor(
+    public translate: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.translate.addLangs(this.availableLanguages);
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('lv');
+    this.translate.setDefaultLang('lv');
 
+    this.sub = this.translate.onLangChange.subscribe(({ lang }: LangChangeEvent) => {
+      // console.log(languages)
+      // this.languages = languages
+    });
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('lv');
+    this.translate.use('lv');
+    this.languages = this.translate.getLangs();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
