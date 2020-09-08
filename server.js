b04ffbca491d0af550562742a1b92e39cc46921e
Martins Kozlovskis <mkozlovskis@gmail.com>
2020-09-08 12:06:27 +0300

    added auto https redirect and page title

--- server.js
+++ server.js
@@ -4,11 +4,24 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      // the statement for performing our redirection
+      return res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/stop-corruption-web-app'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname,'/dist/stop-corruption-web-app/index.html'));
 });
 
